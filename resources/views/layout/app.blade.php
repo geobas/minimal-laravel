3051b235205fd6b8bc6c2a07d26389ff12b72c15
geobas <ksenera@yahoo.com>
2018-06-23 19:58:36 +0300

    Add authorization feature

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,59 +1,78 @@
 <!DOCTYPE html>
-<html data-whatinput="keyboard" data-whatintent="keyboard" class=" whatinput-types-initial whatinput-types-keyboard"><head>
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-        <meta charset="UTF-8">
-        <title>Landon Hotel App</title>
-            <link rel="icon" type="image/x-icon" href="favicon.ico" />
-            <link rel="stylesheet" href="{{ asset('css/foundation.css') }}">
-            <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-            <link rel="stylesheet" href="{{ asset('pickadate/lib/themes/default.css') }}">
-            <link rel="stylesheet" href="{{ asset('pickadate/lib/themes/default.date.css') }}">
-    <meta class="foundation-mq"></head>
-    <body>
+<html data-whatinput="keyboard" data-whatintent="keyboard" class=" whatinput-types-initial whatinput-types-keyboard">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+  <meta charset="UTF-8">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>Landon Hotel App</title>
+  <link rel="icon" type="image/x-icon" href="favicon.ico" />
+  <link rel="stylesheet" href="{{ asset('css/foundation.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('pickadate/lib/themes/default.css') }}">
+  <link rel="stylesheet" href="{{ asset('pickadate/lib/themes/default.date.css') }}">
+  <meta class="foundation-mq">
+</head>
+<body>
 
-        <!-- Start Top Bar -->
-    <div class="top-bar">
-      <div class="row">
-        <div class="top-bar-left">
-          <ul class="dropdown menu" data-dropdown-menu="tckp8q-dropdown-menu" role="menubar">
-            <li role="menuitem"><a href="{{ route('home') }}">Home</a></li>
-            <li role="menuitem"><a href="{{ route('clients') }}">Clients</a></li>
-            <li role="menuitem"><a href="{{ route('reservations') }}">Reservations</a></li>
-          </ul>
-        </div>
+  <!-- Start Top Bar -->
+  <div class="top-bar">
+    <div class="row">
+      <div class="top-bar-left">
+        <ul class="dropdown menu" data-dropdown-menu="tckp8q-dropdown-menu" role="menubar">
+          <li role="menuitem"><a href="{{ route('home') }}">Home</a></li>
+          <li role="menuitem"><a href="{{ route('clients') }}">Clients</a></li>
+          <li role="menuitem"><a href="{{ route('reservations') }}">Reservations</a></li>
+        </ul>
       </div>
+
+      @if (Auth::check())
+      <div class="top-bar-right">
+        <ul class="dropdown menu" data-dropdown-menu="tckp8q-dropdown-menu" role="menubar">
+          <li role="menuitem"><a href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                           Logout
+                                       </a>
+
+                                       <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                           {{ csrf_field() }}
+                                       </form></li>
+        </ul>
+      </div>
+      @endif
     </div>
-    <!-- End Top Bar -->
+  </div>
+  <!-- End Top Bar -->
 
-    <br>
+  <br>
 
-    @yield('content')
+  @yield('content')
 
-    <div class="row column">
-      <hr>
-      <ul class="menu">
-        <li class="float-right">Copyright 2017</li>
-      </ul>
-    </div>
+  <div class="row column">
+    <hr>
+    <ul class="menu">
+      <li class="float-right">Copyright 2017</li>
+    </ul>
+  </div>
 
-    <script>
-      $(document).foundation();
-    </script>
+  <script>
+    $(document).foundation();
+  </script>
 
-        <script src="{{ asset('js/vendor/jquery.js') }}"></script>
-        <script src="{{ asset('js/vendor/what-input.js') }}"></script>
-        <script src="{{ asset('js/vendor/foundation.js') }}"></script>
-        <script src="{{ asset('js/app.js') }}"></script>
-        <script src="{{ asset('pickadate/lib/picker.js') }}"></script>
-        <script src="{{ asset('pickadate/lib/picker.date.js') }}"></script>
-        <script>
-            $('.datepicker').pickadate(
-              {
-                format: 'yyyy-mm-dd',
-                formatSubmit: 'yyyy-mm-dd'
-              }
-              );
-        </script>
+  <script src="{{ asset('js/vendor/jquery.js') }}"></script>
+  <script src="{{ asset('js/vendor/what-input.js') }}"></script>
+  <script src="{{ asset('js/vendor/foundation.js') }}"></script>
+  <script src="{{ asset('js/app.js') }}"></script>
+  <script src="{{ asset('pickadate/lib/picker.js') }}"></script>
+  <script src="{{ asset('pickadate/lib/picker.date.js') }}"></script>
+  <script>
+    $('.datepicker').pickadate(
+    {
+      format: 'yyyy-mm-dd',
+      formatSubmit: 'yyyy-mm-dd'
+    }
+    );
+  </script>
 
-    </body>
+</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,19 +16,22 @@
 //     return view('welcome', $data);
 // });
 
-Route::get('/', 'ContentsController@home')->name('home');
-Route::get('/clients', 'ClientsController@index')->name('clients');
-Route::get('/clients/new', 'ClientsController@new')->name('new_client');
-Route::post('/clients/new', 'ClientsController@create')->name('create_client');
-Route::get('/clients/{id}', 'ClientsController@show')->name('show_client');
-Route::post('/clients/{id}', 'ClientsController@edit')->name('update_client');
-Route::get('/clients/delete/{id}', 'ClientsController@delete')->name('delete_client');
+Route::middleware('auth')->group( function() {
+    Route::get('/', 'ContentsController@home')->name('home');
+    // Route::get('/clients', 'ClientsController@index')->name('clients')->middleware('auth');
+    Route::get('/clients', 'ClientsController@index')->name('clients')->middleware('auth');
+    Route::get('/clients/new', 'ClientsController@new')->name('new_client');
+    Route::post('/clients/new', 'ClientsController@create')->name('create_client');
+    Route::get('/clients/{id}', 'ClientsController@show')->name('show_client');
+    Route::post('/clients/{id}', 'ClientsController@edit')->name('update_client');
+    Route::get('/clients/delete/{id}', 'ClientsController@delete')->name('delete_client');
 
-Route::get('/reservations/{client_id}', 'RoomsController@checkAvailableRooms')->name('check_room');
-Route::post('/reservations/{client_id}', 'RoomsController@checkAvailableRooms')->name('check_room');
+    Route::get('/reservations/{client_id}', 'RoomsController@checkAvailableRooms')->name('check_room');
+    Route::post('/reservations/{client_id}', 'RoomsController@checkAvailableRooms')->name('check_room');
 
-Route::get('/reservations', 'ReservationsController@index')->name('reservations');
-Route::get('/book/room/{client_id}/{room_id}/{date_in}/{date_out}', 'ReservationsController@bookRoom')->name('book_room');
+    Route::get('/reservations', 'ReservationsController@index')->name('reservations');
+    Route::get('/book/room/{client_id}/{room_id}/{date_in}/{date_out}', 'ReservationsController@bookRoom')->name('book_room');
+});
 
 Route::get('/hello', function () {
     return "<h3>Hello world</h3>";
@@ -54,3 +57,8 @@ Route::get('/facades/db', function () {
     return Crypt::decrypt('eyJpdiI6IitOWXJuMGhRenpcL0VmVDIzclFhSlhBPT0iLCJ2YWx1ZSI6Ik1iTzhPR1RKRjlMR09valRlSERPakE9PSIsIm1hYyI6IjdiODU2MTM2NDZiMzE0Mzg2Nzc2NTk4NDUwNjk1OTkzMzY2MGNjMTBiODYxYWJmOGRiNGE1YWVlNWJjZmNkMjYifQ==');
     // return Request::userAgent();
 });
+
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/generate/password', function() { return bcrypt('admin'); });
