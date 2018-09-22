f0c887719eba2adbb22c8534857918ae54c4513e
geobas <ksenera@yahoo.com>
2018-09-22 19:38:17 +0300

    Add form request' for Client@edit

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -23,22 +23,6 @@ class ClientsController extends Controller
 
     public function index()
     {
-        // $obj = new \stdClass;
-        // $obj->id = 1;
-        // $obj->title = 'mr';
-        // $obj->name = 'john';
-        // $obj->last_name = 'doe';
-        // $obj->email = 'john@domain.com';
-        // $data['clients'][] = $obj;
-
-        // $obj = new \stdClass;
-        // $obj->id = 2;
-        // $obj->title = 'ms';
-        // $obj->name = 'jane';
-        // $obj->last_name = 'doe';
-        // $obj->email = 'jane@another-domain.com';
-        // $data['clients'][] = $obj;
-
         $data = [];
         $data['clients'] = $this->client->all();
 
@@ -81,19 +65,15 @@ class ClientsController extends Controller
         $data['titles'] = $this->titles;
         $data['modify'] = 1;
 
-        $client_data = $this->client->find($id);
+        $client_data = $this->client->findOrFail($id);
 
         if ($client_data)
         {
-            $data['client_id'] = $id;
-            $data['client_title'] = $client_data->title;
-            $data['name'] = $client_data->name;
-            $data['last_name'] = $client_data->last_name;
-            $data['address'] = $client_data->address;
-            $data['zip_code'] = $client_data->zip_code;
-            $data['city'] = $client_data->city;
-            $data['state'] = $client_data->state;
-            $data['email'] = $client_data->email;
+            $data = array_merge($data, $client_data->toArray());
+            $data['client_id'] = $data['id'];
+            unset($data['id']);
+            $data['client_title'] = $data['title'];
+            unset($data['title']);
 
             $request->session()->put('last_updated', $client_data->name . ' ' . $client_data->last_name);
         }
@@ -105,47 +85,12 @@ class ClientsController extends Controller
         return view('client/form', $data);
     }
 
-    public function edit($id, Request $request)
+    public function edit($id, UserRequest $request)
     {
-        $data = [];
-
-        $data['title'] = $request->input('title');
-        $data['name'] = $request->input('name');
-        $data['last_name'] = $request->input('last_name');
-        $data['address'] = $request->input('address');
-        $data['zip_code'] = $request->input('zip_code');
-        $data['city'] = $request->input('city');
-        $data['state'] = $request->input('state');
-        $data['email'] = $request->input('email');
-
         if( $request->isMethod('post') )
         {
-            //dd($data);
-            $this->validate(
-                $request,
-                [
-                    'name' => 'required',
-                    'last_name' => 'required|min:5',
-                    'address' => 'required',
-                    'zip_code' => 'required',
-                    'city' => 'required',
-                    'state' => 'required',
-                    'email' => 'required',
-                ]
-            );
-
-            $client_data = $this->client->find($id);
-
-            $client_data->title = $request->input('title');
-            $client_data->name = $request->input('name');
-            $client_data->last_name = $request->input('last_name');
-            $client_data->address = $request->input('address');
-            $client_data->zip_code = $request->input('zip_code');
-            $client_data->city = $request->input('city');
-            $client_data->state = $request->input('state');
-            $client_data->email = $request->input('email');
-
-            $client_data->save();
+            $client = $this->client->findOrFail($id);
+            $client->update($request->all());
 
             return redirect('clients');
         }
