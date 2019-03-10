31370c588074ab9f9d963ff43827f4735a294fc7
geobas <ksenera@yahoo.com>
2019-03-10 23:23:23 +0200

    Update functional tests

--- tests/Feature/ContentsControllerTest.php
+++ tests/Feature/ContentsControllerTest.php
@@ -4,15 +4,38 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\User;
+use Auth;
 
 class ContentsControllerTest extends TestCase
 {
-    public function testHomeAction()
-    {
-        $response = $this->get('/');
+	protected $response;
 
-        $response->assertStatus(200);
-        $this->assertContains('Landon Hotel App', $response->getContent());
-        $response->assertSeeText('West London')->assertDontSee('East London');
+    protected function setUp()
+    {
+     	parent::setUp();
+        $this->response = $this->get('/login');
+        $this->response->assertStatus(200);
     }
+
+    public function testLoginPage()
+    {
+        $this->assertContains('Login', $this->response->getContent());
+        $this->response->assertSeeText('Remember Me')->assertDontSee('The original Landon perseveres');
+    }
+
+    public function testGuestUser()
+    {
+    	$this->assertTrue(Auth::guest());
+    	$this->assertNull(Auth::user());
+   		$this->assertGuest();
+    }
+
+	public function testLoggedInUser()
+	{
+		$user = factory(User::class)->create();
+		$this->actingAs($user)->get('/')->assertStatus(200)->assertSeeText('And the not-to-miss Rooftop Cafe');
+		$this->assertAuthenticated();
+		$this->assertNotNull(Auth::user());
+	}
 }
